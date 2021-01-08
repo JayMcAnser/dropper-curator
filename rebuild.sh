13f0cfef19547d9982c22f88b18959f6d03c087f
Jay Mc Anser <jay@mcanser.com>
2021-01-08 12:05:11 +0100

    add rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,15 @@
+## update the git and restart the server
+
+## get the latest version
+git pull
+
+## update the deps
+npm update
+
+## build the new definition on the server
+npm run build
+
+## should we call pm2 restart DropperCurator ?
+
+## show we did load the latest version?
+pm2 ls
